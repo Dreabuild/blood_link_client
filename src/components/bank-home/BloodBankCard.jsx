c98704a67297fe0926f20b6558319af68a2172d7
Saidur Rahman <saidurhere@gmail.com>
2024-05-18 14:39:29 +0600

    [UPDATED] blood bank

--- src/components/bank-home/BloodBankCard.jsx
+++ src/components/bank-home/BloodBankCard.jsx
@@ -1,42 +1,32 @@
-import Image from "next/image";
 import React from "react";
 import Button from "../ui/Button";
-import Link from "next/link";
+import Image from "next/image";
 
 const Card = () => {
   return (
-    <div className="group border mx-6 my-8 cursor-pointer hover:bg-[#F4F4F4] transition-all p-7">
-      <div className="  ">
-        <div className="flex items-center justify-between">
-          <div className=" flex items-center gap-x-1">
-            <Image
-              src="/assets/icons/blood.svg"
-              alt="Arrow"
-              width={20}
-              height={20}
-            />
-            <p className="font-medium">New Bangladesh Pathology & Blood Bank</p>
-          </div>
-          <p className="text-gray-200 font-semibold">#dhk01</p>
-        </div>
-        <div className="mt-3 flex items-center gap-x-1">
-          <Image
-            src="/assets/icons/location.svg"
-            alt="Arrow"
-            width={20}
-            height={20}
-          />
-          <p className="font-medium text-gray-200">
+    <div className="group border mx-6 my-8 cursor-pointer hover:bg-[#F4F4F4] transition-all p-7 relative">
+      <div className="flex items-center justify-between relative">
+        <div className="w-1/2 relative z-10">
+          <p className="font-semibold text-primary mb-2">#Dhk01</p>
+          <p className="font-medium">New Bangladesh Pathology & Blood Bank</p>
+          <p className="font-medium text-gray-200 mt-2">
             House #69, Road #9/A, Dhanmondi R/A
           </p>
-        </div>
-        <div className="mt-9 flex items-center justify-between">
-          <div className="w-20 space-y-2">
+          <div className="mt-6 flex items-center justify-between w-36 gap-x-2">
             <Button text="Call" className="w-full" />
             <Button text="SMS" className="w-full" />
           </div>
-          <Image src="/assets/map.png" alt="map" width={150} height={90} />
         </div>
+        {/* <div className="absolute inset-0 w-full left-1/2 transform -translate-x-1/2 z-0">
+          <div className="h-full w-full bg-gradient-to-r from-white to-transparent absolute left-0"></div>
+          <Image
+            src="/assets/map.png"
+            alt="map"
+            layout="fill"
+            objectFit="cover"
+            className="opacity-20"
+          />
+        </div> */}
       </div>
     </div>
   );
